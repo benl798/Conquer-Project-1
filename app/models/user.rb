efdcc7583bba205c8abc5d2757725748148c2a9e
benl798 <ben.l7@hotmail.com>
2020-08-12 17:10:50 +1000

    Removed seed data for unused tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   has_many :workouts #one-to-many
 
-  has_and_belongs_to_many :liked_workouts, class_name: 'Workout' #many to many for like/fave. The method name 'workouts' is already taken by the has many association above so we need to rename our HABTM association for liking workouts. We can call it whatever we want, i.e 'liked_workouts' but then we have to tell ActiveRecord what the other class/model is for the other end of this association, since it's no longer possible to work it out automatically from the name.
+  # has_and_belongs_to_many :liked_workouts, class_name: 'Workout' #many to many for like/fave. The method name 'workouts' is already taken by the has many association above so we need to rename our HABTM association for liking workouts. We can call it whatever we want, i.e 'liked_workouts' but then we have to tell ActiveRecord what the other class/model is for the other end of this association, since it's no longer possible to work it out automatically from the name.
 
   has_secure_password # includes some password validation
 
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,7 +1,7 @@
 class Workout < ApplicationRecord
   has_and_belongs_to_many :exercises
 
-  has_and_belongs_to_many :liked_by_users, class_name: 'User' #many to many for like/fave
+  # has_and_belongs_to_many :liked_by_users, class_name: 'User' #many to many for like/fave
 
   belongs_to :user, optional: true
 end
